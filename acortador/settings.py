033b37b9e4c44fd260fa0dbcc2e8abbc4c9f82f6
Darwin Perez Perez <perez.darwin@gmail.com>
2022-11-09 23:19:55 -0500

    changes secret key

--- acortador/settings.py
+++ acortador/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'jqd)wqh@vvivy&8h5fe(iye@7-09k^kq1ujx%+qu9+)vh1cb+3'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
